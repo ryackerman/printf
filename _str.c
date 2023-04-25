5dd8e00d68bc5ff91906c76c3d35285a104cb58e
Khalid <t.khalidtaleb@gmail.com>
2023-04-25 13:11:21 -0700

    fixing str function

--- _str.c
+++ _str.c
@@ -13,3 +13,17 @@ int _strlen(char *s)
 	return (c);
 
 }
+
+/**
+ * _strlenc - Strlen function but applied for constant char pointer s
+ * @s: Type char pointer
+ * Return: c
+ */
+int _strlenc(const char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+		;
+	return (c);
+}
