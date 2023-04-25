068960696816061591f4151571babdad0cd6e88c
Mohamedkor2003 <mohameddkoraichi6635@gmail.com>
2023-04-25 10:39:16 -0700

    updated main h

--- main.h
+++ main.h
@@ -29,5 +29,18 @@ int printf_string(va_list val);
 int _strlen(char *s);
 int _putchar(char c);
 int _printf(const char *format, ...);
-
+int printf_pointer(va_list val);
+int printf_hex_aux(unsigned long int num);
+int printf_exclusive_string(va_list val);
+int printf_hex(va_list val);
+int printf_oct(va_list val);
+int printf_unsigned(va_list args);
+int printf_srev(va_list args);
+int printf_rot13(va_list args);
+int printf_int(va_list args);
+int printf_dec(va_list args);
+int *_strcpy(char *dest, char *src);
+int _strlenc(const char *s);
+int rev_string(char *s);
+int _strlenc(const char *s);
 #endif
